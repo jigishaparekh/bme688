a4a2e9e07e8c5ea24945a432909e26b85eaa097a
jigisha <jigishaparekh99@gmail.com>
2024-11-07 15:54:00 +0530

    Added precompiled lib folder

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "D:/Jigisha/ESP-IDF/bme688_NW/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "C:/Espressif/v5.1.4/esp-idf/.git")
+set(GIT_DIR "D:/Jigisha/ESP-IDF/bme688_NW/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
